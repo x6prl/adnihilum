c5ba3dc6e39b1d9438748a92f75ef014e44bf8a1
Viktor Filinkov <x6prlie@gmail.com>
2026-05-16 10:28:49 +0000

    add CI

--- tools/build_and_run_tests.sh
+++ tools/build_and_run_tests.sh
@@ -3,5 +3,5 @@ set -eu
 
 # run from ..
 mkdir -p build
-"${CC:-cc}" -std=c23 -O0 -pg -g -Wall -Wextra tests/lp_test.c log.c -o build/lp_test -DDEBUG
+"${CC:-cc}" -std=c2x -O0 -pg -g -Wall -Wextra tests/lp_test.c log.c -o build/lp_test -DDEBUG
 ./build/lp_test
